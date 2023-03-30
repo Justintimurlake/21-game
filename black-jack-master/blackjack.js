8a00d75f45498f729c9bbc02c1f3ef63680bcd41
TIMUR ADAMOV <99993754+Justintimurlake@users.noreply.github.com>
2023-03-30 12:34:49 -0700

    Add files via upload

---
+++ black-jack-master/blackjack.js
@@ -0,0 +1,166 @@
+let dealerSum = 0;
+let yourSum = 0;
+
+let dealerAceCount = 0;
+let yourAceCount = 0;
+
+let hidden;
+let deck;
+
+let canHit = true;
+
+window.onload = function () {
+  buildDeck();
+  shuffleDeck();
+  startGame();
+};
+
+function buildDeck() {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let types = ["C", "D", "H", "S"];
+  deck = [];
+
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push(`${values[j]}-${types[i]}`);
+    }
+  }
+  console.log(deck);
+}
+
+function shuffleDeck() {
+  for (let i = 0; i < deck.length; i++) {
+    let j = Math.floor(Math.random() * deck.length);
+    let temp = deck[j];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+}
+
+function startGame() {
+  hidden = deck.pop();
+  dealerSum += getValue(hidden);
+  dealerAceCount += checkAce(hidden);
+  while (dealerSum < 17) {
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    dealerSum += getValue(card);
+    dealerAceCount += checkAce(card);
+    document.getElementById("dealer-cards").append(cardImg);
+  }
+
+  for (let i = 0; i < 2; i++) {
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    yourSum += getValue(card);
+    yourAceCount += checkAce(card);
+    document.getElementById("your-cards").append(cardImg);
+  }
+  document.getElementById("hit").addEventListener("click", hit);
+  document.getElementById("stay").addEventListener("click", stay);
+}
+
+function hit() {
+  if (!canHit) {
+    return;
+  }
+
+  let cardImg = document.createElement("img");
+  let card = deck.pop();
+  cardImg.src = "./cards/" + card + ".png";
+  yourSum += getValue(card);
+  yourAceCount += checkAce(card);
+  document.getElementById("your-cards").append(cardImg);
+
+  if (reduceAce(yourSum, yourAceCount) > 21) {
+    canHit = false;
+  }
+}
+
+function stay() {
+  dealerSum = reduceAce(dealerSum, dealerAceCount);
+  yourSum = reduceAce(yourSum, yourAceCount); // Fixed the error here
+
+  canHit = false;
+  document.getElementById("hidden").src =
+    "./cards/" + hidden + ".png";
+
+  let message = "";
+  if (yourSum > 21) {
+    message = "You Lose";
+  } else if (dealerSum > 21) {
+    message = "You win";
+  } else if (dealerSum == yourSum) {
+    message = "Tie";
+  } else if (yourSum > dealerSum) {
+    message = "You Win";
+  } else if (yourSum < dealerSum) {
+    message = "You Lose!";
+  }
+  document.getElementById("dealer-sum").innerText = dealerSum;
+  document.getElementById("your-sum").innerText = yourSum;
+  document.getElementById("results").innerText = message;
+  setTimeout(reloadGame, 2000); // Keep only this line for reloading the game
+}
+
+function getValue(card) {
+  let data = card.split("-");
+  let value = data[0];
+
+  if (isNaN(value)) {
+    if (value == "A") {
+      return 11;
+    }
+    return 10;
+  }
+  return parseInt(value);
+}
+
+function checkAce(card) {
+  if (card[0] == "A") {
+    return 1;
+  }
+  return 0;
+}
+
+function reduceAce(playerSum, playerAceCount) {
+  while (playerSum > 21 && playerAceCount > 0) {
+    playerSum -= 10; // Fixed the error here
+    playerAceCount -= 1;
+  }
+  return playerSum;
+}
+
+function reloadGame() {
+  //   dealerSum = 0;
+  //   yourSum = 0;
+  //   dealerAceCount = 0;
+  //   yourAceCount = 0;
+  //   canHit = true;
+
+  //   document.getElementById("dealer-cards").innerHTML = "";
+  //   document.getElementById("your-cards").innerHTML = "";
+  //   document.getElementById("dealer-sum").innerText = "";
+  //   document.getElementById("your-sum").innerText = "";
+  //   document.getElementById("results").innerText = "";
+
+  //   buildDeck();
+  //   shuffleDeck();
+  location.reload();
+}
